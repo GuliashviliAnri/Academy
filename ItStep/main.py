04cd7f327326514d8449bb7ea15f552a6178b9c8
Guliashvili <Kylo@Anris-MacBook-Air.local>
2024-05-13 21:49:50 +0400

    test

--- ItStep/main.py
+++ ItStep/main.py
@@ -1,5 +1,10 @@
-def main():
-    pass
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return render_template("Academy.html")
 
 
-main()
+app.run(debug=True)
